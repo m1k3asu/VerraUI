c43d8ea9b0fad0d1b7237a23b5794af18ff29dbb
Michael Martinez <m1k3asu@gmail.com>
2023-11-11 06:33:01 -0600

    update readme. Add logic to retrieve master country list

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IDisplayList } from '../models/display-list';
+import { VerraService } from '../service/verra.service';
+import { SubscriptionLike as ISubscription } from "rxjs";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
 
-  displayList: IDisplayList[] = [];
+  temp:any;
+  countryList: string[] = [];
+  selectedCountry = '';
+
+  constructor(private verraService: VerraService) {}
+
+  
+  ngOnInit() {
+ 
+    this.getMasterCountryList();
+  }
+
+
+
+  getMasterCountryList() {
+    this.verraService.getMasterCountryList()
+      .subscribe(
+        (response) => {          
+          console.log('response received');
+          this.temp = response;
+          this.extractValues(this.temp.data, "name");
+        },
+        (error) => {                              //Error callback
+          console.error('error caught in component');
+          //throw error;   //You can also throw the error to a global error handler
+        }
+      )
+  }
+
+  extractValues(arr: any, prop: string) {
+    let extractValues = arr.map((item: { [x: string]: any; }) => item[prop]);
+    // prepend ---- to the list
+    Array.prototype.unshift.apply( extractValues, ["----"]);
+    this.countryList = extractValues;
+  } 
+  selectCountryHandler(event: any) {
+    this.selectedCountry =  event.target.value as string;
+  }
 
 }
+
+
