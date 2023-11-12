eed383b84d1ea3b30a3fbec364e67e08632f739f
Michael Martinez <m1k3asu@gmail.com>
2023-11-12 15:33:30 -0600

    finalize for verra interview

--- src/app/models/display-list.ts
+++
@@ -1,5 +0,0 @@
-export interface IDisplayList {
-    // name: number;
-    // ProjectName: string;
-    // Country: string
-}
--- src/app/service/verra.service.ts
+++ src/app/service/verra.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
+import { IProjectDetails } from '../models/project-details';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,23 @@ export class VerraService {
 
   getMasterCountryList() {
     return this.http.get(this.countryUrl);             
+  }
+
+  getRegisteredList(): Observable<any> {
+    return this.http.get(this.baseApiUrl + "api/Verra/GetRegisteredList")
+  }
+
+  private getHeaders() {
+    const headers = new HttpHeaders().set('ContentType', 'application/json');  
+    headers.append("Access-Control-Allow-Origin", "*");
+    headers.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS, DELETE");
+    headers.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Origin, X-Requested-With, Content-Type, Accept");
+    return headers;
+  }
+
+  addProjectAndCountry(projectDetail: IProjectDetails): Observable<any> {
+    const headers = this.getHeaders();       
+    return this.http.post<any>(this.baseApiUrl + "api/Verra/AddProject", projectDetail,{ headers: headers});
   }
 
 }
